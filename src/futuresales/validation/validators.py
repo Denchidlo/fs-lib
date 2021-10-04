26e25a9e232c1f4ab65a44f96627d7587d6920a7
denchidlo <twihkapb@gmail.com>
2021-10-04 19:45:00 +0300

    FS-LIB_0.1.9[Patch]: Patch changes:  - Added residual creator

--- src/futuresales/validation/validators.py
+++ src/futuresales/validation/validators.py
@@ -21,3 +21,10 @@ class Validator:
         truth = y
 
         return np.sqrt(mse(predictions, truth)), self._train_err,  predictions, truth
+
+def get_statistics(report):
+    stat = report[3]
+    stat['residuals'] = report[2].transpose()[0] - report[3].valid_target
+    stat['predicted'] = report[2]
+    stat['object_id'] = report[3].index
+    return stat
